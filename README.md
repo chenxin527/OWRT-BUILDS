1368b962641885e6d3c25a53580048419a534279
chenxin <chenxin_verified@163.com>
2025-08-15 17:29:42 +0800

    第一次提交

---
+++ README.md
@@ -0,0 +1,23 @@
+# OWRT-Builds
+
+本仓库修改自以下仓库：https://github.com/VIKINGYFY/OpenWRT-CI
+
+云编译OpenWRT固件
+
+官方版：
+https://github.com/immortalwrt/immortalwrt.git
+
+高通版：
+https://github.com/VIKINGYFY/immortalwrt.git
+
+# 固件简要说明：
+
+固件信息里的时间为编译开始的时间，方便核对上游源码提交时间。
+
+# 目录简要说明：
+
+workflows——自定义CI配置
+
+Scripts——自定义脚本
+
+Config——自定义配置
---
+++ Scripts/Handles.sh
@@ -0,0 +1,97 @@
+#!/bin/bash
+
+PKG_PATH="$GITHUB_WORKSPACE/wrt/package/"
+
+#预置HomeProxy数据
+if [ -d *"homeproxy"* ]; then
+	echo " "
+
+	HP_RULE="surge"
+	HP_PATH="homeproxy/root/etc/homeproxy"
+
+	rm -rf ./$HP_PATH/resources/*
+
+	git clone -q --depth=1 --single-branch --branch "release" "https://github.com/Loyalsoldier/surge-rules.git" ./$HP_RULE/
+	cd ./$HP_RULE/ && RES_VER=$(git log -1 --pretty=format:'%s' | grep -o "[0-9]*")
+
+	echo $RES_VER | tee china_ip4.ver china_ip6.ver china_list.ver gfw_list.ver
+	awk -F, '/^IP-CIDR,/{print $2 > "china_ip4.txt"} /^IP-CIDR6,/{print $2 > "china_ip6.txt"}' cncidr.txt
+	sed 's/^\.//g' direct.txt > china_list.txt ; sed 's/^\.//g' gfw.txt > gfw_list.txt
+	mv -f ./{china_*,gfw_list}.{ver,txt} ../$HP_PATH/resources/
+
+	cd .. && rm -rf ./$HP_RULE/
+
+	cd $PKG_PATH && echo "homeproxy date has been updated!"
+fi
+
+#修改argon主题字体和颜色
+if [ -d *"luci-theme-argon"* ]; then
+	echo " "
+
+	cd ./luci-theme-argon/
+
+	sed -i "/font-weight:/ { /important/! { /\/\*/! s/:.*/: var(--font-weight);/ } }" $(find ./luci-theme-argon -type f -iname "*.css")
+	sed -i "s/primary '.*'/primary '#31a1a1'/; s/transparency '.*'/transparency '0.5'/; s/transparency_dark '.*'/transparency_dark '0.5'/; s/mode '.*'/mode 'normal'/; s/online_wallpaper '.*'/online_wallpaper 'none'/" ./luci-app-argon-config/root/etc/config/argon
+
+	cd $PKG_PATH && echo "theme-argon has been fixed!"
+fi
+
+#修改qca-nss-drv启动顺序
+NSS_DRV="../feeds/nss_packages/qca-nss-drv/files/qca-nss-drv.init"
+if [ -f "$NSS_DRV" ]; then
+	echo " "
+
+	sed -i 's/START=.*/START=85/g' $NSS_DRV
+
+	cd $PKG_PATH && echo "qca-nss-drv has been fixed!"
+fi
+
+#修改qca-nss-pbuf启动顺序
+NSS_PBUF="./kernel/mac80211/files/qca-nss-pbuf.init"
+if [ -f "$NSS_PBUF" ]; then
+	echo " "
+
+	sed -i 's/START=.*/START=86/g' $NSS_PBUF
+
+	cd $PKG_PATH && echo "qca-nss-pbuf has been fixed!"
+fi
+
+#修复TailScale配置文件冲突
+TS_FILE=$(find ../feeds/packages/ -maxdepth 3 -type f -wholename "*/tailscale/Makefile")
+if [ -f "$TS_FILE" ]; then
+	echo " "
+
+	sed -i '/\/files/d' $TS_FILE
+
+	cd $PKG_PATH && echo "tailscale has been fixed!"
+fi
+
+#修复Rust编译失败
+RUST_FILE=$(find ../feeds/packages/ -maxdepth 3 -type f -wholename "*/rust/Makefile")
+if [ -f "$RUST_FILE" ]; then
+	echo " "
+
+	sed -i 's/ci-llvm=true/ci-llvm=false/g' $RUST_FILE
+
+	cd $PKG_PATH && echo "rust has been fixed!"
+fi
+
+#集成OpenClash内核
+if [ -d *"openclash"* ]; then
+	CLASH_CORE_DIR=$PKG_PATH/luci-app-openclash/root/etc/openclash/core
+	echo " "
+	echo Downloading clash-linux-"$CPU_MODEL".tar.gz...
+	curl -sL -m 30 --retry 2 https://raw.githubusercontent.com/vernesong/OpenClash/core/master/meta/clash-linux-"$CPU_MODEL".tar.gz -o /tmp/clash.tar.gz
+	echo Download completed, extracting...
+	tar zxvf /tmp/clash.tar.gz -C /tmp >/dev/null 2>&1
+	chmod +x /tmp/clash >/dev/null 2>&1
+	mkdir -p $CLASH_CORE_DIR
+	mv /tmp/clash $CLASH_CORE_DIR/clash_meta >/dev/null 2>&1
+	rm -rf /tmp/clash.tar.gz >/dev/null 2>&1
+	if [ -x "$CLASH_CORE_DIR/clash_meta" ]; then
+		echo "OpenClash core has been installed!"
+	else
+		echo "OpenClash core hasn't been installed correctly!"
+	fi
+	cd $PKG_PATH
+fi
